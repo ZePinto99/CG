43ce96e377cd93df282b9d61c815577b69e52ef1
ZePinto99 <z.miguelpinto@gmail.com>
2020-04-26 23:07:02 +0100

    Funções curvas e CreateVBO

--- TP_Fase3/Build/main.cpp
+++ TP_Fase3/Build/main.cpp
@@ -21,6 +21,125 @@ std::vector<Ponto> triangles;
 vector<OperFile*> files; // Vector de OperFiles (que relacionam os ficheiros 
 					     // com as suas respetivas transformações).
 
+GLuint vertexCount;
+GLuint buffers;
+float* vertexB;
+
+////////////////////////////////Curvas///////////////////////////////////////
+#define POINT_COUNT 5
+// Points that make up the loop for catmull-rom interpolation
+float p[POINT_COUNT][3] = { {-1,-1,0},{-1,1,0},{1,1,0},{0,0,0},{1,-1,0} };
+
+void buildRotMatrix(float* x, float* y, float* z, float* m) {
+
+	m[0] = x[0]; m[1] = x[1]; m[2] = x[2]; m[3] = 0;
+	m[4] = y[0]; m[5] = y[1]; m[6] = y[2]; m[7] = 0;
+	m[8] = z[0]; m[9] = z[1]; m[10] = z[2]; m[11] = 0;
+	m[12] = 0; m[13] = 0; m[14] = 0; m[15] = 1;
+}
+
+
+void cross(float* a, float* b, float* res) {
+
+	res[0] = a[1] * b[2] - a[2] * b[1];
+	res[1] = a[2] * b[0] - a[0] * b[2];
+	res[2] = a[0] * b[1] - a[1] * b[0];
+}
+
+
+void normalize(float* a) {
+
+	float l = sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2]);
+	a[0] = a[0] / l;
+	a[1] = a[1] / l;
+	a[2] = a[2] / l;
+}
+
+
+float length(float* v) {
+
+	float res = sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+	return res;
+
+}
+
+void multMatrixVector(float* m, float* v, float* res) {
+
+	for (int j = 0; j < 4; ++j) {
+		res[j] = 0;
+		for (int k = 0; k < 4; ++k) {
+			res[j] += v[k] * m[j * 4 + k];
+		}
+	}
+
+}
+
+
+void getCatmullRomPoint(float t, float* p0, float* p1, float* p2, float* p3, float* pos, float* deriv) {
+
+	// catmull-rom matrix
+	float m[4][4] = { {-0.5f,  1.5f, -1.5f,  0.5f},
+						{ 1.0f, -2.5f,  2.0f, -0.5f},
+						{-0.5f,  0.0f,  0.5f,  0.0f},
+						{ 0.0f,  1.0f,  0.0f,  0.0f} };
+	float p[4];
+	float a[4];
+	for (int i = 0; i < 4; i++) {
+		// Compute A = M * P
+		p[0] = p0[i];
+		p[1] = p1[i];
+		p[2] = p2[i];
+		p[3] = p3[i];
+		multMatrixVector(*m, p, a);
+
+		// Compute pos[i] = T * A
+		pos[i] = pow(t, 3) * a[0] + pow(t, 2) * a[1] + t * a[2] + a[3];
+
+		// Compute pos[i] = T * A
+		deriv[i] = 3 * pow(t, 2) * a[0] + 2 * t * a[1] + a[2];
+	}
+}
+
+
+
+// given  global t, returns the point in the curve
+void getGlobalCatmullRomPoint(float gt, float* pos, float* deriv) {
+
+	float t = gt * POINT_COUNT; // this is the real global t
+	int index = floor(t);  // which segment
+	t = t - index; // where within  the segment
+
+	// indices store the points
+	int indices[4];
+	indices[0] = (index + POINT_COUNT - 1) % POINT_COUNT;
+	indices[1] = (indices[0] + 1) % POINT_COUNT;
+	indices[2] = (indices[1] + 1) % POINT_COUNT;
+	indices[3] = (indices[2] + 1) % POINT_COUNT;
+
+	getCatmullRomPoint(t, p[indices[0]], p[indices[1]], p[indices[2]], p[indices[3]], pos, deriv);
+}
+
+void renderCatmullRomCurve() {
+
+	// draw curve using line segments with GL_LINE_LOOP
+	float p[4][3] = { { 0.0f, 0.0f, 0.0f}, // A
+					  { 1.0f, 0.0f, 1.0f}, // B
+					  {-1.0f, 0.0f, 1.0f}, // C
+					  {-1.0f, 0.0f, 0.0f} }; // D
+
+	float pos[4];
+	float deriv[4];
+	glBegin(GL_LINE_LOOP);
+	for (float gt = 0; gt < 1; gt += 0.01) {
+		getGlobalCatmullRomPoint(gt, pos, deriv);
+		glVertex3f(pos[0], pos[1], pos[2]);
+	}
+	glEnd();
+}
+
+////////////////////////////////////////////////////////////////
+
+
 void lerficheiro(std::string nomeficheiro)
 {
 	int c = 0;
@@ -50,8 +169,34 @@ void lerficheiro(std::string nomeficheiro)
 			triangles.push_back(aux);
 			c = 0;
 		}
-		//	}
+		/*
+		glBindBuffer(GL_ARRAY_BUFFER, buffers);
+		glBufferData(GL_ARRAY_BUFFER, vertexCount, vertexB, GL_STATIC_DRAW);
+		glVertexPointer(3, GL_DOUBLE, 0, 0);
+		glDrawArrays(GL_TRIANGLES, 0, vertexCount);
+
+		glPopMatrix();
+		*/
 	}
+}
+
+void createVBO() {
+	vertexB = (float*)malloc(sizeof(float) * triangles.size() * 3);
+	int i = 0;  int vertex = 0;
+
+	vector<Ponto>::iterator tri = triangles.begin();
+
+	for (int j = 0; tri != triangles.end(); j++, vertex++) {
+		vertexB[i] = tri[j].x;
+		i++;
+		vertexB[i] = tri[j].y;
+		i++;
+		vertexB[i] = tri[j].z;
+
+		vertex++;
+	}
+
+	vertexCount = vertex;
 }
 
 
@@ -186,6 +331,7 @@ int main(int argc, char** argv)
 {
 	// put GLUT’s init here
 	xmlParser("config.xml", files);
+	createVBO();
 	//if (!files.empty()) std::cout << "ola";
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
